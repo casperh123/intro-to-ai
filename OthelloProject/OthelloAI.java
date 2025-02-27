001a1c860ef60f22c8383b1daaa8d3c31c3e3e69
casperh123 <casperholten@me.com>
2025-02-27 21:36:59 +0100

    Something something pseudocode

--- OthelloProject/OthelloAI.java
+++ OthelloProject/OthelloAI.java
@@ -1,24 +1,72 @@
 package OthelloProject;
 
+import jdk.jshell.execution.Util;
+
 public class OthelloAI implements IOthelloAI {
     public OthelloAI() {
     }
 
-    public Position MinimMax(GameState state) {
-
-        Position optimalPosition = null;
-        //player <- game.TO-MOVE(state)
-        //value, move <- MAX-VALUE(game, state)
-
-        return optimalPosition;
+    public Position MiniMax(GameState state) {
+        return MaxValue(state).move;
     }
 
-    public Position MaxValue(GameState state) {
-        return null;
+    public UtilityMove MaxValue(GameState state) {
+        if(state.isFinished()) {
+            return new UtilityMove(Utility(state), null);
+        }
+
+        UtilityMove utilityMove = new UtilityMove(Integer.MIN_VALUE, null);
+
+        for(Position position : state.legalMoves()) {
+            state.insertToken(position);
+
+            UtilityMove secondMove = MinValue(state);
+
+            if(secondMove.utility > utilityMove.utility) {
+                utilityMove = new UtilityMove(secondMove.utility, position);
+            }
+        }
+
+        return utilityMove;
+    }
+
+    public UtilityMove MinValue(GameState state) {
+        if(state.isFinished()) {
+            return new UtilityMove(Utility(state), null);
+        }
+
+        UtilityMove utilityMove = new UtilityMove(Integer.MIN_VALUE, null);
+
+        for(Position position : state.legalMoves()) {
+            state.insertToken(position);
+
+            UtilityMove secondMove = MaxValue(state);
+
+            if(secondMove.utility < utilityMove.utility) {
+                utilityMove = new UtilityMove(secondMove.utility, position);
+            }
+        }
+
+        return utilityMove;
+    }
+
+    public int Utility(GameState state) {
+        return 1;
     }
 
     @Override
     public Position decideMove(GameState s) {
-        return MinimMax(s);
+        Position position = MiniMax(s);
+        return position;
+    }
+
+    public class UtilityMove {
+        public int utility;
+        public Position move;
+
+        public UtilityMove(int utility, Position move) {
+            this.utility = utility;
+            this.move = move;
+        }
     }
 }
