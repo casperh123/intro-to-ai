6d76e346f1017de1aa76552855dbc95e0aa6a561
casperh123 <casperholten@me.com>
2025-02-27 20:21:11 +0100

    init othello

---
+++ OthelloProject/OthelloProject/OthelloAI.java
@@ -0,0 +1,12 @@
+package OthelloProject.Opponents;
+
+import OthelloProject.GameState;
+import OthelloProject.IOthelloAI;
+import OthelloProject.Position;
+
+public class OthelloAI implements IOthelloAI {
+    @Override
+    public Position decideMove(GameState s) {
+        return null;
+    }
+}
